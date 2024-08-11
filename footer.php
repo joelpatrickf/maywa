caca191ea08e3cd9d1f4587e0a6ed2bac3a6d24d
Joel Patrick <joel.patrickf@gmail.com>
2024-08-11 02:05:29 -0500

    LAST

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+    <script src="Dependencies/jquery.min.js"></script>
+    <script src="Dependencies/jquery.easing.min.js"></script>
+    <script src="Dependencies/bootstrap.min.js"></script>
+    <script src="Dependencies/masterslider.min.js"></script>
+    <script src="Dependencies/owl.carousel.min.js"></script>
+    <script src="main.js"></script>
