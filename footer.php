93cb6c5893a5eca257f1ce700be15179bb9f4fc5
Joel Patrick <joel.patrickf@gmail.com>
2024-08-19 23:55:33 -0500

    fixed

--- footer.php
+++
@@ -1,7 +0,0 @@
-
-    <script src="Dependencies/jquery.min.js"></script>
-    <script src="Dependencies/jquery.easing.min.js"></script>
-    <script src="Dependencies/bootstrap.min.js"></script>
-    <script src="Dependencies/masterslider.min.js"></script>
-    <script src="Dependencies/owl.carousel.min.js"></script>
-    <script src="main.js"></script>
